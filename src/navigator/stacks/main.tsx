8244e9d9577f00db8f6d85188d5cbb636ac9ad36
Ehsan Bigzad <ehsanbigzad7@gmail.com>
2022-12-20 22:06:20 +0430

    fix: bottom tab icons

--- src/navigator/stacks/main.tsx
+++ src/navigator/stacks/main.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, {type ComponentType} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import {
+  Camera,
+  IconProps,
+  Calculator,
+  Stickynote,
+  DirectNotification,
+} from 'iconsax-react-native';
+
+import {colors, font} from 'styles';
+import {mScale} from 'styles/mixins';
 
 import HomeScreen from 'screens/home';
 import MemoScreen from 'screens/memo';
@@ -10,11 +21,66 @@ const Tab = createBottomTabNavigator();
 
 export default function MainStack() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Photo" component={PhotoScreen} />
-      <Tab.Screen name="Memo" component={MemoScreen} />
-      <Tab.Screen name="Calculator" component={CalculatorScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          fontSize: font.xs,
+          fontWeight: '500',
+        },
+        tabBarInactiveTintColor: colors.primary.t40,
+        tabBarActiveTintColor: colors.primary.default,
+        tabBarStyle: {
+          elevation: 0,
+          height: mScale(60),
+          paddingTop: mScale(5),
+          paddingBottom: mScale(5),
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: props => getIcon(props, DirectNotification),
+        }}
+      />
+      <Tab.Screen
+        name="Photo"
+        component={PhotoScreen}
+        options={{
+          tabBarIcon: props => getIcon(props, Camera),
+        }}
+      />
+      <Tab.Screen
+        name="Memo"
+        component={MemoScreen}
+        options={{
+          tabBarIcon: props => getIcon(props, Stickynote),
+        }}
+      />
+      <Tab.Screen
+        name="Calculator"
+        component={CalculatorScreen}
+        options={{
+          tabBarIcon: props => getIcon(props, Calculator),
+        }}
+      />
     </Tab.Navigator>
+  );
+}
+
+type TabBarProps = {
+  size: number;
+  color: string;
+  focused: boolean;
+};
+
+function getIcon(props: TabBarProps, Icon: ComponentType<IconProps>) {
+  const {focused} = props;
+
+  return (
+    <Icon
+      size={mScale(27.5)}
+      color={focused ? colors.primary.default : colors.primary.t30}
+    />
   );
 }
