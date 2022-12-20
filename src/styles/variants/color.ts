32a14930236e9097209628c4ca78e1b0666542f0
Ehsan Bigzad <ehsanbigzad7@gmail.com>
2022-12-20 15:12:45 +0430

    feat: define colors

---
+++ src/styles/variants/color.ts
@@ -0,0 +1,113 @@
+const gray = {
+  s50: '#fafafa',
+  s100: '#f5f5f5',
+  s200: '#e5e5e5',
+  s300: '#d4d4d4',
+  s400: '#a3a3a3',
+  s500: '#737373',
+  s600: '#525252',
+  s700: '#404040',
+  s800: '#262626',
+  s900: '#171717',
+};
+
+const success = {
+  s50: '#ecfdf5',
+  s100: '#d1fae5',
+  s200: '#a7f3d0',
+  s300: '#6ee7b7',
+  s400: '#34d399',
+  s500: '#10b981',
+  s600: '#059669',
+  s700: '#047857',
+  s800: '#065f46',
+  s900: '#064e3b',
+};
+
+const danger = {
+  s50: '#fff1f2',
+  s100: '#ffe4e6',
+  s200: '#fecdd3',
+  s300: '#fda4af',
+  s400: '#fb7185',
+  s500: '#f43f5e',
+  s600: '#e11d48',
+  s700: '#be123c',
+  s800: '#9f1239',
+  s900: '#881337',
+};
+
+const warning = {
+  s50: '#fffbeb',
+  s100: '#fef3c7',
+  s200: '#fde68a',
+  s300: '#fcd34d',
+  s400: '#fbbf24',
+  s500: '#f59e0b',
+  s600: '#d97706',
+  s700: '#b45309',
+  s800: '#92400e',
+  s900: '#78350f',
+};
+
+const info = {
+  s50: '#f0f9ff',
+  s100: '#e0f2fe',
+  s200: '#bae6fd',
+  s300: '#7dd3fc',
+  s400: '#38bdf8',
+  s500: '#0ea5e9',
+  s600: '#0284c7',
+  s700: '#0369a1',
+  s800: '#075985',
+  s900: '#0c4a6e',
+};
+
+const primary = {
+  t90: '#eaebf0',
+  t80: '#d5d6e1',
+  t70: '#bfc2d1',
+  t60: '#aaaec2',
+  t50: '#959ab3',
+  t40: '#8085a4',
+  t30: '#6b7195',
+  t20: '#555d85',
+  t10: '#404876',
+  default: '#2b3467',
+  s10: '#272f5d',
+  s20: '#222a52',
+  s30: '#1e2448',
+  s40: '#1a1f3e',
+  s50: '#161a34',
+  s60: '#111529',
+  s70: '#0d101f',
+  s80: '#090a15',
+  s90: '#04050a',
+};
+
+const secondary = '#EB455F';
+
+const tertiary1 = '#FCFFE7';
+const tertiary2 = '#BAD7E9';
+
+const black = '#000000';
+const white = '#ffffff';
+
+const transparent = 'transparent';
+
+const colors = {
+  gray,
+  success,
+  danger,
+  warning,
+  info,
+  primary,
+  secondary,
+  tertiary1,
+  tertiary2,
+  black,
+  white,
+  transparent,
+};
+
+export default colors;
