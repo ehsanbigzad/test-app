494bdfc7b4cff4642dedaea3875581f6e0fed385
Ehsan Bigzad <ehsanbigzad7@gmail.com>
2022-12-20 15:24:39 +0430

    chore: apply navigation theme

--- src/navigator/index.tsx
+++ src/navigator/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import theme from 'styles/theme';
 import MainStack from './stacks/main';
 import AuthStack from './stacks/auth';
 
@@ -15,7 +16,7 @@ export default function RootNavigator({onReady}: RootNavigatorProps) {
   const isAuth = true;
 
   return (
-    <NavigationContainer onReady={onReady}>
+    <NavigationContainer onReady={onReady} theme={theme}>
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
