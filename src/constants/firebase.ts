8e1c48394b5525222a24a73cdc19ec018ecd4c7a
Ehsan Bigzad <ehsanbigzad7@gmail.com>
2022-12-21 11:53:49 +0430

    feat: add firebase error types

---
+++ src/constants/firebase.ts
@@ -0,0 +1,6 @@
+export const firebaseError = {
+  EMAIL_IN_USE: 'auth/email-already-in-use',
+  INVALID_EMAIL: 'auth/invalid-email',
+  NOT_FOUND: 'auth/user-not-found',
+  WRONG_PASSWORD: 'auth/wrong-password',
+};
