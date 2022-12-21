a9285abb0e9c904cc2a1a937bec1faf8c6455785
Ehsan Bigzad <ehsanbigzad7@gmail.com>
2022-12-22 02:12:30 +0430

    fix: messaging

--- index.js
+++ index.js
@@ -5,8 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './src/app';
 import {name as appName} from './app.json';
-import {onBackgroundNotification} from 'utils/notification';
-
-onBackgroundNotification();
 
 AppRegistry.registerComponent(appName, () => App);
