3636829a115065dadb88f9518d4df1c464932da4
Ehsan Bigzad <ehsanbigzad7@gmail.com>
2022-12-21 22:55:48 +0430

    feat: memo screen

--- src/screens/memo/index.tsx
+++ src/screens/memo/index.tsx
@@ -1,38 +1,101 @@
-import React, {useCallback} from 'react';
-import {View, Text, StyleSheet, Pressable} from 'react-native';
-import notifee from '@notifee/react-native';
+import React, {useState} from 'react';
+import {StyleSheet, ScrollView, ToastAndroid} from 'react-native';
+import {Controller, useForm} from 'react-hook-form';
+import firestore from '@react-native-firebase/firestore';
+
+import Button from 'ui/button';
+import TextBox from 'ui/text-box';
+import {mScale} from 'styles/mixins';
+import useAuthUser from 'hooks/auth-user';
+import KeyboardShift from 'components/keyboard-shift';
+
+interface MemoFormData {
+  memo: string;
+}
 
 export default function MemoScreen() {
-  const showNotification = useCallback(async () => {
-    const channelId = await notifee.createChannel({
-      id: 'default',
-      name: 'Default Channel',
-    });
+  const [loading, setLoading] = useState<boolean>(false);
+  const {user} = useAuthUser();
 
-    await notifee.displayNotification({
-      title: 'Notification Title',
-      body: 'Main body content of the notification',
-      android: {
-        channelId,
-        pressAction: {
-          id: 'default',
-        },
-      },
-    });
-  }, []);
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<MemoFormData>();
+
+  const onSubmit = handleSubmit(async payload => {
+    setLoading(true);
+
+    const memoCollection = firestore().collection('Memo');
+    const memoObjects = await memoCollection
+      .where('uid', '==', user?.uid)
+      .get();
+
+    try {
+      // Create new record
+      if (memoObjects.empty) {
+        await memoCollection.add({uid: user?.uid, note: payload.memo});
+        ToastAndroid.show('Note saved successfully.', ToastAndroid.SHORT);
+        return;
+      }
+
+      // Update record
+      const documentId = memoObjects.docs[0].id;
+      await memoCollection.doc(documentId).update({
+        note: payload.memo,
+      });
+
+      ToastAndroid.show('Note updated successfully.', ToastAndroid.SHORT);
+    } catch (_) {
+      ToastAndroid.show('Something went wrong, try later.', ToastAndroid.SHORT);
+    }
+
+    setLoading(false);
+  });
 
   return (
-    <View style={styles.container}>
-      <Pressable onPress={showNotification}>
-        <Text>Show notification</Text>
-      </Pressable>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <KeyboardShift>
+        <Controller
+          name="memo"
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: 'Enter a memo.',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextBox
+              label="Note"
+              value={value}
+              onBlur={onBlur}
+              autoComplete="off"
+              keyboardType="default"
+              onChangeText={onChange}
+              returnKeyType="default"
+              autoCapitalize="sentences"
+              error={errors.memo?.message}
+              description="This note will be persisted in firestore database"
+            />
+          )}
+        />
+        <Button
+          label="Save"
+          onPress={onSubmit}
+          loading={loading}
+          containerStyle={styles.button}
+        />
+      </KeyboardShift>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: mScale(25),
+  },
+  button: {
+    marginTop: mScale(15),
   },
 });
